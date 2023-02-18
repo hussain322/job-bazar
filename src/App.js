1484706dfea085077284c176267e1f7bfbbcd799
hussain322 <hussain.ahmed8768@gmail.com>
2023-02-18 08:25:56 +0600

    install tailwind and daisyui

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ function App() {
     <div className="App">
       <button className="btn">Button</button>
       <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
     </div>
   );
 }
